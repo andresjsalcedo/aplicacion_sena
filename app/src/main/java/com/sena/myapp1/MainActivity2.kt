24588adb1a279ca7322eced0b820d30699aa9d60
jose osorio <josemiguelosorio1234@gmail.com>
2024-08-02 22:09:43 -0500

    se agregaron pantallas de inicio de sesion y creacion de cuenta

---
+++ app/src/main/java/com/sena/myapp1/MainActivity2.kt
@@ -0,0 +1,28 @@
+package com.sena.myapp1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btn: Button = findViewById(R.id.boton_login)
+        btn.setOnClickListener {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+    }
+}
+    }
---
+++ app/src/main/java/com/sena/myapp1/RECUPERAR_CONTRA.kt
@@ -0,0 +1,28 @@
+package com.sena.myapp1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RECUPERAR_CONTRA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recuperar_contra)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textView: TextView = findViewById(R.id.rec)
+        textView.setOnClickListener {
+            val intent: Intent = Intent(this, RECUPERAR_CONTRA::class.java)
+            startActivity(intent)
+    }
+}
+}
